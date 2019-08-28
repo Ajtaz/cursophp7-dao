95d928aeb4230e9708e106baf4806bf62960ac68
Ajtaz <aureliojazevedo@gmail.com>
2019-08-28 16:38:07 -0300

    Métodos de lista.

--- index.php
+++ index.php
@@ -12,11 +12,35 @@ echo json_encode($usuarios);
 
 */
 
+/* 
 $root = new Usuario();
 
-$root->loadbyId(3);
+$root->loadbyId(9);
 
 echo $root;
 
+*/
+
+/*
+// Lista de usuários
+
+$lista = Usuario::getList();  // posso usar assim porque essa função é estatica. Não é preciso instanciar. 
+
+echo json_encode($lista);
+
+*/
+
+/*
+$search = Usuario::search("jo");
+echo json_encode($search);
+
+*/
+
+// carrega usuário usando logi e senha 
+
+$usuario = new Usuario();
+$usuario->login("user2", "12345678");
+
+echo $usuario;
 
 ?>
